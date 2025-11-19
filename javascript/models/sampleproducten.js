21c53b3d19c4d37551ee6008d5fad146c83da92d
lorewuyt <lorewuytjens@leerling.wico.be>
2025-11-19 11:42:27 +0100

    structerele aanpassing lore

---
+++ javascript/models/sampleproducten.js
@@ -0,0 +1,28 @@
+export class Sampledata {
+    constructor() {
+        this.producten = [
+            {
+                Id: 1,
+                naam: 'Beta rr 50',
+                prijs: 4000,
+                afbeelding: "afbeeldingen/betarr50.webp",
+            },
+
+            {
+                Id: 2,
+                naam: 'H2R',
+                prijs: 6000,
+                afbeelding: "afbeeldingen/H2R.jpg",
+            },
+
+            {
+                Id: 3,
+                naam: 'BMW',
+                prijs: 25000,
+                afbeelding: "afbeeldingen/s1krr.jpg",
+            }
+
+        ]
+    }
+
+}
